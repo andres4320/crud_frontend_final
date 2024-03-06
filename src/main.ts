b23a1f5f066273254adac0baf6737998b6e9885b
andres4320 <sergio.andres.garzon17@gmail.com>
2024-03-06 16:59:55 -0500

    hola2

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,18 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr } from 'ngx-toastr';
 
-bootstrapApplication(AppComponent, appConfig)
+// Combina las configuraciones de la aplicación y las de Toastr
+const combinedConfig = {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []), // Mantén los proveedores existentes si los hay
+    provideAnimations(), // Proveedores de animaciones requeridos
+    provideToastr(), // Proveedores de Toastr
+  ],
+};
+
+bootstrapApplication(AppComponent, combinedConfig)
   .catch((err) => console.error(err));
